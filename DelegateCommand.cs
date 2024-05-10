5d351a70a3c6f10c8826894e7abb51b5d3b39a06
Kathy Bunn <mckathlin@gmail.com>
2024-05-10 14:02:43 -0400

    Add MVVM.DelegateCommand, which implements ICommand

---
+++ DelegateCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace MVVM
+{
+    class DelegateCommand : ICommand
+    {
+        private readonly Action _action;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public DelegateCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
