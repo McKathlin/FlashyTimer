c71b3a3ba4865ec02308a2e4682776953333fa16
Kathy Bunn <mckathlin@gmail.com>
2024-05-10 13:31:27 -0400

    Add TimerViewModel as a stub.

---
+++ TimerViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Media;
+
+namespace FlashyTimer
+{
+    public class TimerViewModel : INotifyPropertyChanged
+    {
+        #region Private Members
+
+        private TimeSpan _timeRemaining;
+        private Color _backgroundColor;
+
+        #endregion
+        #region Properties
+
+        public TimeSpan TimeRemaining
+        {
+            get
+            {
+                return _timeRemaining;
+            }
+            set
+            {
+                _timeRemaining = value;
+                OnPropertyChanged(nameof(TimeRemaining));
+            }
+        }
+
+        public Color BackgroundColor
+        {
+            get
+            {
+                return _backgroundColor;
+            }
+            set
+            {
+                _backgroundColor = value;
+                OnPropertyChanged(nameof(BackgroundColor));
+            }
+        }
+
+        #endregion
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
