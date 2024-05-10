a035a5e657f5b311431bcf55d6ec89aec21b1e93
Kathy Bunn <mckathlin@gmail.com>
2024-05-10 15:19:07 -0400

    Change TimerViewModel's background into a Brush

--- TimerViewModel.cs
+++ TimerViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using System.Windows.Media;
 using MVVM;
 
@@ -10,33 +11,65 @@ namespace FlashyTimer
 {
     public class TimerViewModel : ObservableObject
     {
-        private TimeSpan _timeRemaining;
-        private Color _backgroundColor;
+        public const string TIME_STOPPED_TEXT = "--:--";
 
-        public TimeSpan TimeRemaining
+        public static readonly Brush REGULAR_BRUSH =
+            new SolidColorBrush(Colors.White);
+        public static readonly Brush WARNING_BRUSH =
+            new SolidColorBrush(Colors.Yellow);
+        public static readonly Brush CRITICAL_BRUSH =
+            new SolidColorBrush(Colors.Red);
+        public static readonly Brush DISABLED_BRUSH =
+            new SolidColorBrush(Colors.LightGray);
+
+        private string _text = TIME_STOPPED_TEXT;
+        private Brush _background = DISABLED_BRUSH;
+
+        private ICommand _startFortyMinutesCommand = null;
+
+        private FlashyTimer.Timer _timer = new FlashyTimer.Timer();
+
+        public string Text
         {
             get
             {
-                return _timeRemaining;
+                return _text;
             }
             set
             {
-                _timeRemaining = value;
-                OnPropertyChanged(nameof(TimeRemaining));
+                _text = value;
+                OnPropertyChanged(nameof(Text));
             }
         }
 
-        public Color BackgroundColor
+        public Brush Background
         {
             get
             {
-                return _backgroundColor;
+                return _background;
             }
             set
             {
-                _backgroundColor = value;
-                OnPropertyChanged(nameof(BackgroundColor));
+                _background = value;
+                OnPropertyChanged(nameof(Background));
             }
+        }
+
+        public ICommand StartFortyMinutesCommand
+        {
+            get
+            {
+                if (null == _startFortyMinutesCommand)
+                {
+                    return new DelegateCommand(StartFortyMinutes);
+                }
+                return _startFortyMinutesCommand;
+            }
+        }
+
+        private void StartFortyMinutes()
+        {
+            _timer.Start(TimeSpan.FromMinutes(40));
         }
 
 
